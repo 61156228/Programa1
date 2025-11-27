011a7f9a49ff517b4cf6430ad1dd187a7ff41674
Flor <61156228@continental.edu.pe>
2025-11-27 13:20:23 -0500

    Codigo de progrma 1

--- programa1.py
+++ programa1.py
@@ -7,3 +7,6 @@ n = int(input("Ingresa el número de término que quieres (n): "))
 an = a1 + (n - 1) * d
 
 print("El término número", n, "de la progresión aritmética es:", an)
+
+Sn = n(an + an)/2
+print("La suma total es: /t",Sn) 
